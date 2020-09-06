31d6cc320d16fbeb3d4e572c75d86b1b889e2fce
Christian <67313472+chriuxenman1@users.noreply.github.com>
2020-09-06 14:53:05 +0200

    Update RNN_Last.py

--- RNN_Last.py
+++ RNN_Last.py
@@ -1,39 +1,30 @@
 # -*- coding: utf-8 -*-
-"""RNN/LSTM Lastprognose
-
-Mega Hinweis: WENN ICH NUR 24h in die Zukunft blicken will, brauche ich keine 3 Jahre an Daten!
-Es reicht vielleicht der letzte Monat! Sensitivitätsanalyse machen!
-
-Schauen ob ich einen kleinen Zeitraum finde, in dem alles vorkommt (nzht, zht, at, ...)
-
-Weniger Daten sparen Trainingszeit.
-
-Mit df.info() zähle ich direkt Datenlücken nan im Datensatz.
-"""
-#%% Vorbereitung
+"""RNN/LSTM Electrical Load prediction"""
+#%% Packages
 import keras # You can safely ignore any warnings on importing this
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+#%% Import load data in 15 min intervall
 df = pd.read_csv('data_prepared.csv', index_col=0, sep=',', parse_dates=True, 
                  infer_datetime_format=True, dayfirst=True)
 df.index.freq = '15T'
 
-# Datenmenge verkleinern auf Tagebasis
+# Reduce data amount to from 2 years to 10 d (to improve train speed)
 data_days = 10
 df = df[-data_days*96:]
 
-# Hges vom Rest trennen
+# Extract the relevant data "Hges"
 df_bck = df
 df = df['Hges']
-df = df.to_frame() # Umwandlung von ser zu df nötig
+df = df.to_frame() # Conversion from series to df necessary
 
 #%% Train Test Split
 train = df.iloc[:-96]
 test = df.iloc[-96:]
 
-#%% Skalierung (0 bis 1)
+#%% Normalize data (0 to 1)
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler() 
 scaler.fit(train)
@@ -43,16 +34,12 @@ scaled_test = scaler.transform(test)
 #%% Time Series Generator
 from keras.preprocessing.sequence import TimeseriesGenerator
 # define generator
-n_input = 96 # Ich gebe dir die letzten 96 Datenpunkte (=1 Tag) und du sagst mir wie die nächsten
-# 15 min aussehen (97. Datenpunkt)
-# Mit n_input kann man ausprobieren
+n_input = 96 # calculate with 96 time stamps
 batch_size = 1
-n_features = 1 # Wieviele Datenspalten man hat steht hier (nur Hges)
-# 2 x scaled_train, weil die Datenreihe und die target Werte eingegeben werden müssen
-train_generator = TimeseriesGenerator(scaled_train, scaled_train, length=n_input, 
-                                      batch_size=batch_size)
+n_features = 1 # univariate time series (without additional features)
+train_generator = TimeseriesGenerator(scaled_train, scaled_train, length=n_input, batch_size=batch_size)
 
-#%% KNN Modell initiieren
+#%% Create LSTM-model
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
 
@@ -63,38 +50,18 @@ model.add(LSTM(256, return_sequences=True))
 model.add(LSTM(128, return_sequences=True))
 model.add(LSTM(64, return_sequences=True))
 model.add(LSTM(n_features, return_sequences=True)) 
-
 model.compile(loss='mse', optimizer='adam')
 
-"""
-# Modell initiieren
-model = Sequential()
-# LST Layer einbauen mit x LSTM Neuronen
+print(model.summary()) # Model summary
 
-model.add(LSTM(128, activation='relu', input_shape=(n_input, n_features), 
-               return_sequences=True))
-
-model.add(LSTM(128, return_sequences=False))
-model.add(LSTM(64, return_sequences=False))
-
-# Zu einem Output Wert zusammenführen
-model.add(Dense(1))
-# Compile
-model.compile(optimizer='adam', loss='mse')
-"""
-
-# Summary des Modells:
-print(model.summary())
-
-
-#%% Modell trainieren
+#%% Train Model
 num_epochs = 1000
 model.fit_generator(train_generator, epochs=num_epochs)
-# Fehler in Abhängigkeit der Epochen plotten
+# Plot the loss per epoch (Alternative use TensorBoard)
 loss_per_epoch = model.history.history['loss']
 plt.plot(range(len(loss_per_epoch)),loss_per_epoch)
 
-#%% Prognose erstellen
+#%% Create prediction
 test_predictions = []
 first_eval_batch = scaled_train[-n_input:]
 current_batch = first_eval_batch.reshape((1, n_input, n_features))
@@ -104,17 +71,16 @@ for i in range(len(test)):
     test_predictions.append(current_pred) 
     current_batch = np.append(current_batch[:,1:,:],[[current_pred]],axis=1)
     
-# Scaling Prozess invertieren
+# Invert normalization
 true_predictions = scaler.inverse_transform(test_predictions)
 test['Predictions'] = true_predictions
 
-#%% Vergleichen
+#%% Compare
 from custom_fun import calculate_stats
 title = "LSTM Neurons, " + str(num_epochs) + " Epochs)"
 # np.nan bei AIC Berechnung, da ich die Anzahl der Parameter nicht kenne hier
 calculate_stats(title, test['Hges'], test['Predictions'], len(train), np.nan) 
 test.plot()
 
-#%% Modell speichern und laden
-# Damit man dieses Modell wieder aufrufen kann
-model.save('RNN_Lastmodell.h5')
+#%% Save Model
+model.save('LSTM_LoadPrediction.h5')
